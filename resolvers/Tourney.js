266375718d4b9d70fd81fa800e62dbb18207adbe
Javier Ocampo <ferroxido@gmail.com>
2017-09-22 00:30:13 -0300

    Implement cursor based paginate for tourneys

--- resolvers/Tourney.js
+++ resolvers/Tourney.js
@@ -3,13 +3,43 @@ const TourneyType = require('../models/TourneyType')
 
 const tourneyResolvers = {
   Query: {
-    tourneys: (_, args) => {
-      let cursor = args.after ? Buffer.from(args.after, 'base64 ').toString('ascii') : 'a'
+    tourneys: async (_, args) => {
+      let cursor = args.after ? Buffer.from(args.after, 'base64').toString('ascii') : 'a'
       let limit = args.limit ? args.limit : 10
 
-      const tourneys = await Tourney.query().where('name', '>', cursor).limit(limit).eager('[tourney_type, user]')
-      const totalCount = await Tourney.query()
-      console.log(tourneys)
+      // Get tourneys
+      const tourneys = await Tourney.query().orderBy('name').where('name', '>', cursor).limit(limit).eager('[tourney_type, user]')
+
+      // Fill edges array
+      const edges = await tourneys.map((tourney) => {
+        return {
+          cursor: Buffer.from(tourney.name).toString('base64'),
+          node: tourney
+        }
+      })
+
+      // Get total rows
+      const total = await Tourney.query().count().first()
+
+      // Calculate hasNextPage
+      let hasNextPage = false
+      let endCursor = tourneys.length > 0 ? tourneys[tourneys.length - 1].name : ''
+      if (endCursor) {
+        const restRows = await Tourney.query().orderBy('name').where('name', '>', endCursor)
+        endCursor = Buffer.from(endCursor).toString('base64')
+        if (restRows.length > 0) {
+          hasNextPage = true
+        }
+      }
+
+      return {
+        totalCount: total.count,
+        edges,
+        pageInfo: {
+          endCursor,
+          hasNextPage
+        }
+      }
     },
     tourney: (_, args) => Tourney.query().eager('[tourney_type, user]').findById(args.id),
     tourneysTypes: () => TourneyType.query().eager('tourneys'),
